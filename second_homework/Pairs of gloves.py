1f8cd2105b1ac35fe31fba49a9ce39d4a95d6b94
maximfreerider <33199854+maximfreerider@users.noreply.github.com>
2018-07-26 18:27:00 +0300

    Update Pairs of gloves.py

--- second_homework/Pairs of gloves.py
+++ second_homework/Pairs of gloves.py
@@ -1,6 +1,6 @@
 #https://www.codewars.com/kata/pair-of-gloves/train/python
 def number_of_pairs(gloves):
-    my_set = set(gloves)
+    unic_gloves = set(gloves)
     total = 0
     for i in my_set:
         total += gloves.count(i)//2
